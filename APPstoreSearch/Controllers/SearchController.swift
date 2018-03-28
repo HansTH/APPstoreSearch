8b94bea97cf6dfc2c8216d27b4e0852f0423fae6
Hans ter Horst <hans@MacBook-Pro-van-Hans.local>
2018-03-28 11:01:39 +0200

    Create rootViewController in AppDelegate.

---
+++ APPstoreSearch/Controllers/SearchController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchController.swift
+//  APPstoreSearch
+//
+//  Created by Hans ter Horst on 28-03-18.
+//  Copyright © 2018 Hans ter Horst. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .red
+        
+    }
+
+
+}
+
